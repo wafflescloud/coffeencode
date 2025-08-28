53436dd6c7911e29a20cf304b515aab5ffbf973a
GitHub Action <action@github.com>
2025-08-28 02:21:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/wafflescloud/coffeencode/MyBundle.kt
+++ src/main/kotlin/com/github/wafflescloud/coffeencode/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wafflescloud.coffeencode
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/wafflescloud/coffeencode/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/wafflescloud/coffeencode/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.wafflescloud.coffeencode.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wafflescloud.coffeencode.MyBundle
+import com.github.wafflescloud.coffeencode.services.MyProjectService
 import javax.swing.JButton
 
 
